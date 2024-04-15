5762aac315370e624da6452bbe10b0a67b7712a8
D-prasad1999 <105363557+D-prasad1999@users.noreply.github.com>
2024-04-15 11:55:03 +0530

    Add files via upload

---
+++ employee-batch-service-csvtodb/src/test/java/com/employeebatchservice/app/EmployeeBatchServiceCsvtodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employeebatchservice.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeBatchServiceCsvtodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
