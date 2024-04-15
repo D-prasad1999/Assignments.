33d9f04d60a993777c6d6ed6f0bde330c3786405
D-prasad1999 <105363557+D-prasad1999@users.noreply.github.com>
2024-04-15 11:58:08 +0530

    Add files via upload

---
+++ employee-batch-service-dbtoxml/src/main/java/com/employeebatchservice/app/EmployeeBatchServiceDbtocsvApplication.java
@@ -0,0 +1,13 @@
+package com.employeebatchservice.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeBatchServiceDbtocsvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeBatchServiceDbtocsvApplication.class, args);
+	}
+
+}
---
+++ employee-batch-service-dbtoxml/src/main/java/com/employeebatchservice/app/configuration/EmployeeBatchConfiguration.java
@@ -0,0 +1,77 @@
+package com.employeebatchservice.app.configuration;
+
+import javax.sql.DataSource;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.database.JdbcCursorItemReader;
+import org.springframework.batch.item.xml.StaxEventItemWriter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+import com.employeebatchservice.app.entity.Employee;
+
+@Configuration
+@EnableBatchProcessing
+public class EmployeeBatchConfiguration {
+
+    @Autowired
+    private JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    private StepBuilderFactory stepBuilderFactory;
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Bean
+    public JdbcCursorItemReader<Employee> reader() {
+        JdbcCursorItemReader<Employee> reader = new JdbcCursorItemReader<>();
+        reader.setDataSource(dataSource);
+        reader.setSql("SELECT id, name, salary, age FROM employee");
+        reader.setRowMapper(new BeanPropertyRowMapper<>(Employee.class));
+        return reader;
+    }
+
+    @Bean
+	public EmployeeProcessor processor() {
+		return new EmployeeProcessor();
+	}
+
+    @Bean
+    public StaxEventItemWriter<Employee> writer() {
+        StaxEventItemWriter<Employee> writer = new StaxEventItemWriter<>();
+        writer.setResource(new FileSystemResource("src/main/resources/employees.xml"));
+        writer.setRootTagName("employees");
+        writer.setMarshaller(new Jaxb2Marshaller() {{
+            setClassesToBeBound(Employee.class);
+        }});
+        return writer;
+	}
+
+    @Bean
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+                .<Employee, Employee>chunk(10)
+                .reader(reader())
+                .processor(processor())
+                .writer(writer())
+                .build();
+    }
+
+    @Bean
+    public Job exportEmployeeJob() {
+        return jobBuilderFactory.get("export-employees")
+                .incrementer(new RunIdIncrementer())
+                .flow(step1())
+                .end()
+                .build();
+    }
+}
